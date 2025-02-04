de407e45d4a3166829dcbdd965d9f3f34d396345
oleg1625 <olegdobrylev95@gmail.com>
2025-02-04 16:14:33 +0300

    Разработка

--- Victorina.py
+++ Victorina.py
@@ -1,7 +1,22 @@
-from tkinter import *
+import pygame
+import sys
 
-root = Tk()
-root.title('Викторина "Женщины в Великой Отечественой войне"')
-root.geometry("1280x720")
+pygame.init()
 
-root.mainloop()
+#Настройка окна
+width = 1280
+height = 720
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Викторина "Женщины Великой Отечественной войны"')
+
+#Цвета
+white = (255, 255, 255)
+black = (0, 0, 0)
+gray = (200, 200, 200)
+green = (0, 255, 0)
+red = (255, 0, 0)
+
+#Шрифты
+font1 = pygame.font.SysFont("Times New Roman", 24, bold = True)
+font2 = pygame.font.SysFont("Times New Roman", 20)
+font3 = pygame.font.SysFont("Times New Roman", 30, bold = True)
